2e31f10d338772bd1b497c157c808d1c8036f5a2
eppdalencon <eppdalencon@gmail.com>
2023-07-24 15:37:17 -0300

    Criando projeto

---
+++ logicKidsProject/logicKidsProject/logicKidsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  logicKidsProjectApp.swift
+//  logicKidsProject
+//
+//  Created by Eduardo Dalencon on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct logicKidsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
