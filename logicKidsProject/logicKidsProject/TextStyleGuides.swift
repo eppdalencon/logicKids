3b4a9ed8f1da7c1bf19a8a3217593e47a0fbc023
gstv <gustavodiefenbach@gmail.com>
2023-07-24 16:33:19 -0300

    adicionei arquivo com a estrutura do text style guides

---
+++ logicKidsProject/logicKidsProject/TextStyleGuides.swift
@@ -0,0 +1,14 @@
+//
+//  ColorGuides.swift
+//  logicKidsProject
+//
+//  Created by Gustavo Diefenbach on 24/07/23.
+//
+import SwiftUI
+
+public extension Font {
+    
+    struct DesignTextSystem {
+        public static let largeTitleBold = Font.system(size: 32, weight: .bold)
+    }
+}
