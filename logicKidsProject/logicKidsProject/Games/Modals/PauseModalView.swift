5a06d48b2174b890ed66e45d5763fd89317c6c32
eppdalencon <eppdalencon@gmail.com>
2023-07-26 14:38:08 -0300

    Criando telas de instrução	, pause e jogo completo.

--- logicKidsProject/logicKidsProject/Games/Modals/PauseModalView.swift
+++ logicKidsProject/logicKidsProject/Games/Modals/PauseModalView.swift
@@ -9,8 +9,67 @@ import SwiftUI
 
 struct PauseModalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:30){
+            VStack{
+                Button(action: {
+                   print("Restart clicado")
+                }) {
+                    
+                    Text("Restart")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                
+                Image("placeholder")
+                    .resizable()
+                    
+            }
+           
+           
+            
+            VStack{
+                Button(action: {
+                   print("Continue clicado")
+                }) {
+                    
+                    Text("Continue")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                
+                Image("placeholder")
+                    .resizable()
+            }
+            
+            
+            VStack{
+                Button(action: {
+                   print("Quit clicado")
+                }) {
+                    
+                    Text("Quit")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                
+                Image("placeholder")
+                    .resizable()
+            }
+            
+        }
+        .padding(30)
+        .frame(width: 500, height: 250)
+        .background(Color.green)
+        .cornerRadius(20)
+        
     }
+    
 }
 
 struct PauseModalView_Previews: PreviewProvider {
