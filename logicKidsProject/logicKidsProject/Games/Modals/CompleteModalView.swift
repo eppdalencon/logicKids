ccde4b0b75b684defa4c81c9701daa41b5163dbd
eppdalencon <eppdalencon@gmail.com>
2023-07-31 15:44:19 -0300

    Criação de navegação tela de jogo completo

--- logicKidsProject/logicKidsProject/Games/Modals/CompleteModalView.swift
+++ logicKidsProject/logicKidsProject/Games/Modals/CompleteModalView.swift
@@ -1,47 +1,50 @@
-//
-//  CompleteModalView.swift
-//  logicKidsProject
-//
-//  Created by Eduardo Dalencon on 24/07/23.
-//
-
 import SwiftUI
 
 struct CompleteModalView: View {
+    var dismissComplete: (() -> Void)
+    var dismissGame: (() -> Void)
+    
     var body: some View {
-        VStack{
-            Text("Congratulations!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            HStack (spacing: 20){
-                Button(action: {
-                   print("Retry clicado")
-                }) {
+        ZStack {
+            Color.black.opacity(0.5).ignoresSafeArea() // Add a semi-transparent background
+            VStack {
+                Text("Congratulations!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                HStack (spacing: 20){
+                    Button(action: {
+                        dismissComplete()
+                    }) {
+                        Text("Retry")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
                     
-                    Text("Retry")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-                
-                Button(action: {
-                    print("New game clicado")
-                }) {
-                    
-                    Text("New Game")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                    Button(action: {
+                        withAnimation {
+                            dismissGame()
+                            dismissComplete()
+                        }
+                    }) {
+                        Text("New Game")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
                 }
             }
+            
+             // Set the frame to take the whole screen
         }
+        .background(Color.red)
     }
 }
 
 struct CompleteModalView_Previews: PreviewProvider {
     static var previews: some View {
-        CompleteModalView()
+        CompleteModalView(dismissComplete: {}, dismissGame: {})
     }
 }
