74c1964fa506be5a2aed8020b55e35f12d095126
eppdalencon <eppdalencon@gmail.com>
2023-08-02 17:10:34 -0300

    Tela de achievements. Adição de imagem na tela de completo. Navegação Achievements

--- logicKidsProject/logicKidsProject/Games/Modals/CompleteModalView.swift
+++ logicKidsProject/logicKidsProject/Games/Modals/CompleteModalView.swift
@@ -4,6 +4,7 @@ struct CompleteModalView: View {
     var dismissComplete: (() -> Void)
     var dismissGame: (() -> Void)
     var shuffleGame: (() -> Void)
+    var gameName: Int
     
     var body: some View {
         
@@ -13,45 +14,62 @@ struct CompleteModalView: View {
                 VStack{
                     Spacer().frame(height: geometry.size.height * 0.07)
                     VStack {
-                        Spacer()
-                        HStack (spacing: 30){
-                           
+                        ZStack{
+                            Image("game\(gameName)0")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: geometry.size.height * 0.5)
+                                .padding(.bottom,geometry.size.height * 0.08)
                             
-                            HomeButton(title: "Quit", color: Color.red, largura: geometry.size.width * 0.2, altura: geometry.size.height * 0.1, action: {
-                                dismissGame();dismissComplete()
-                            })
                             
-                            HomeButton(title: "Play Again", color: Color.blue, largura: geometry.size.width * 0.2, altura: geometry.size.height * 0.1, action: {
-                                shuffleGame();dismissComplete()
-                            })
-//                            Button(action: {
-//                                withAnimation {
-//                                    dismissGame()
-//                                    dismissComplete()
-//                                }
-//                            }) {
-//                                Text("Quit")
-//                                    .padding()
-//                                    .foregroundColor(.white)
-//                                    .background(Color.blue)
-//                                    .cornerRadius(10)
-//                            }
-//                            
-//                            Button(action: {
-//                                shuffleGame()
-//                                dismissComplete()
-//                            }) {
-//                                Text("Play Again")
-//                                    .padding()
-//                                    .foregroundColor(.white)
-//                                    .background(Color.blue)
-//                                    .cornerRadius(10)
-//                            }
+                            VStack{
+                                Spacer()
+                                HStack (spacing: 30){
+                                   
+                                    
+                                    
+                                    HomeButton(title: "Quit", color: Color.red, largura: geometry.size.width * 0.2, altura: geometry.size.height * 0.1, action: {
+                                        dismissGame();dismissComplete()
+                                    })
+                                    
+                                    HomeButton(title: "Play Again", color: Color.blue, largura: geometry.size.width * 0.2, altura: geometry.size.height * 0.1, action: {
+                                        shuffleGame();dismissComplete()
+                                    })
+        //                            Button(action: {
+        //                                withAnimation {
+        //                                    dismissGame()
+        //                                    dismissComplete()
+        //                                }
+        //                            }) {
+        //                                Text("Quit")
+        //                                    .padding()
+        //                                    .foregroundColor(.white)
+        //                                    .background(Color.blue)
+        //                                    .cornerRadius(10)
+        //                            }
+        //
+        //                            Button(action: {
+        //                                shuffleGame()
+        //                                dismissComplete()
+        //                            }) {
+        //                                Text("Play Again")
+        //                                    .padding()
+        //                                    .foregroundColor(.white)
+        //                                    .background(Color.blue)
+        //                                    .cornerRadius(10)
+        //                            }
+                                }
+                                .padding(.bottom,geometry.size.height * 0.0)
+                            }
                         }
+                        
+                        
+                       
+                        
                     }
                     
                     .padding(30)
-                    .frame(width: geometry.size.width / 1.5, height: geometry.size.height / 1.5)
+                    .frame(width: geometry.size.width / 1.5, height: geometry.size.height / 1.3)
                     .background(Color("lightBackground"))
                     .overlay(
                         RoundedRectangle(cornerRadius: 20)
@@ -78,6 +96,6 @@ struct CompleteModalView: View {
 
 struct CompleteModalView_Previews: PreviewProvider {
     static var previews: some View {
-        CompleteModalView(dismissComplete: {}, dismissGame: {}, shuffleGame: {})
+        CompleteModalView(dismissComplete: {}, dismissGame: {}, shuffleGame: {}, gameName: 0)
     }
 }
